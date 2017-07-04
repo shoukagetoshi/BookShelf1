289f63dad95e41509e7bc82db0222344240d0964
shou kagetoshi <shoukagetoshi@gmail.com>
2017-07-04 13:28:48 +0000

    4-21 kaminari終了！

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'books#index'
-  resources :books
+  resources :books do
+    get 'page/:page' , :action => :index, :on => :collection
+  end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
